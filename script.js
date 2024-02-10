95306123b60f73594a50e7d62941b69d9e7c82b9
lukeregalado <125326219+lukeregalado@users.noreply.github.com>
2024-02-10 19:07:28 +0800

    added student/lab tech feature to register

--- script.js
+++ script.js
@@ -4,18 +4,42 @@ const register = document.querySelector('.register-link');
 const openLogin = document.querySelector('.login-popup')
 const closeLogin = document.querySelector('.close-login')
 
+// clears all input fields
+function resetLoginRegisterFields() {
+    document.getElementById('login-email').value = "";
+    document.getElementById('login-pw').value = "";
+    document.getElementById('reg-email').value = "";
+    document.getElementById('reg-pw').value = "";
+    document.getElementById('reg-confirm-pw').value = "";
+}
+
+// executes when Register is clicked
 register.addEventListener('click', ()=> {
-    wrap.classList.add('active');
+    wrap.classList.add('register-as');
 });
 
+// executes when user chooses Student or Lab Technician
+function registerAs(r) {
+    wrap.classList.add('active');
+    wrap.classList.remove('register-as');
+    resetLoginRegisterFields()
+}
+
+// executes when Login is clicked
 login.addEventListener('click', ()=> {
     wrap.classList.remove('active');
+    resetLoginRegisterFields()
 });
 
+
+// executes when Login (topright of main-content) is clicked
 openLogin.addEventListener('click', ()=> {
     wrap.classList.add('popup');
 });
 
+// executes when close (X) button is clicked
 closeLogin.addEventListener('click', ()=> {
-    wrap.classList.remove('popup');
+    wrap.className = "wrap";
+    resetLoginRegisterFields()
 });
+
