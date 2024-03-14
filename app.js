c7bf12e0e58f25cae89b9637852336b70b49b469
Luke <luke.m.regalado@gmail.com>
2024-03-15 00:06:21 +0800

    Profile Features Functional (CRUD)

--- app.js
+++ app.js
@@ -19,28 +19,138 @@ server.use(express.static(path.join(__dirname, 'public')));
 /*
 <================ MAIN CODE ================>
 */
-
-
 server.get('/', function(req, res){
     res.render('main', {layout : 'index'});
  
- });
-
-
-
-server.get('/profile', function(req, res){
-   res.render('profile', {layout : 'index', reservations: reservations, userData: userData["im_nayeon@dlsu.edu.ph"] })
 });
 
-// for user search
-server.get('/userNames', function(req, res) {
-   // list names of all users
-   const userNames = Object.values(userData).map(user => user.name);
+// register user
+server.post('/register', (req, res) => {
+   const username = req.body.email;
+   const userPassword = req.body.password;
+   const userPosition = req.body.pos;
 
-   // send user names to the client-side code
-   res.json(userNames);
+   if (userData[username]) {
+      console.log(`User with email ${username} already exists!.`);
+      res.status(400).send('User already exists!');
+  } else {
+      userData[username] = {
+         name: username,
+         password: userPassword,
+         pos: userPosition,
+         pfpURL: "/images/sample-users/Name",
+         email: username,
+         description: "This user hasn't added a description yet."
+      }
+  }
+   console.log('add', userData);
+   res.status(200).send('Profile registered successfully');
 });
 
+
+/*
+<================ PROFILE ================>
+*/
+server.get('/profile', function(req, res) {
+   renderProfilePage(res, null, userData["im_nayeon@dlsu.edu.ph"]);
+});
+
+
+// search user
+server.get('/profile/:username', (req, res) => {
+   const username = req.params.username;
+   // get user data given email
+   const userInfo = userData[findUserEmailByName(username)]; 
+   
+   console.log('Redirecting to ', username, '\'s profile...');
+   
+   if (userInfo) {
+      // render the profile page with userData
+      renderProfilePage(res, null, userInfo);
+   } else {
+      // handle error
+      res.status(404).send('User not found');
+   }
+});
+
+// edit user
+server.post('/edit-profile', (req, res) => {
+   const userEmail = req.body.email;
+   const username = req.body.name;
+   const userDesc = req.body.description;
+   if (userData[userEmail]) {
+      userData[userEmail].name = username;
+      userData[userEmail].description = userDesc;
+      console.log(`User with email ${userEmail} edited successfully.`);
+  } else {
+      console.log(`User with email ${userEmail} not found.`);
+  }
+   res.status(200).send('Profile edited successfully');
+});
+
+server.post('/search', (req, res) => {
+   const inputData = req.body.data;
+   console.log('Received live search input:', inputData);
+
+   const searchSuggestions = filterUserData(inputData);
+ 
+   res.status(200).json(searchSuggestions);
+});
+
+// delete user
+server.post('/delete-profile', (req, res) => {
+   const userEmail = req.body.email;
+   if (userData[userEmail]) {
+      // delete the user from the userData object
+      delete userData[userEmail];
+      console.log(`User with email ${userEmail} deleted successfully.`);
+  } else {
+      console.log(`User with email ${userEmail} not found.`);
+  }
+   res.status(200).send('Profile deleted successfully');
+});
+
+server.post('/search', (req, res) => {
+   const inputData = req.body.data;
+   console.log('Received live search input:', inputData);
+
+   const searchSuggestions = filterUserData(inputData);
+ 
+   res.status(200).json(searchSuggestions);
+});
+
+
+// helper function for searching userData given name
+function findUserEmailByName(name) {
+   for (const [email, userInfo] of Object.entries(userData)) {
+       if (userInfo.name === name) {
+           return email; // return email
+       }
+   }
+   // if no match
+   return null;
+}
+
+// helper function for rendering profile page
+function renderProfilePage(res, suggestions, userInfo) {
+   res.render('profile', {
+       layout: 'index',
+       reservations: reservations,
+       userData: userInfo,
+       suggestions: suggestions
+   });
+}
+
+// helper function for user search
+function filterUserData(inputString) {
+   const filteredUsers = [];
+   for (const [key, value] of Object.entries(userData)) {
+       if (value.name.toLowerCase().includes(inputString.toLowerCase())) {
+            filteredUsers.push({ name: value.name, pfpURL: value.pfpURL, email: value.email });
+        };
+   }
+   return filteredUsers;
+}
 
 server.get('/vsa', function(req, res){
    res.render('vsa', {layout : 'index'});
@@ -49,7 +159,6 @@ server.get('/vsa', function(req, res){
 server.get('/reserveslot', function(req, res){
    res.render('reserveslot', {layout : 'index'});
 });
-
 
 server.get('/seereservation', function(req, res){
    res.render('seereservation', {layout : 'index'});
@@ -67,36 +176,46 @@ let reservations = [
 ];
 
 let userData = {
-   "im_nayeon@dlsu.edu.ph": {
-       name: "Im Na-yeon",
-       pfpURL: "/images/sample-users/Im Na-yeon",
-       email: "im_nayeon@dlsu.edu.ph",
-       description: "Student at DLSU University. Lead vocalist of TWICE."
-   },
-   "yoo_jeongyeon@dlsu.edu.ph": {
-       name: "Yoo Jeong-yeon",
-       pfpURL: "/images/sample-users/Yoo Jeong-yeon",
-       email: "yoo_jeongyeon@dlsu.edu.ph",
-       description: "Lab Technician at a certain DLSU Lab. Main vocalist of TWICE."
-   },
-   "hirai_momo@dlsu.edu.ph": {
-       name: "Hirai Momo",
-       pfpURL: "/images/sample-users/Hirai Momo",
-       email: "hirai_momo@dlsu.edu.ph",
-       description: "Lab Technician at a certain DLSU Lab. Main dancer of TWICE."
-   },
-   "minatozaki_sana@dlsu.edu.ph": {
-       name: "Minatozaki Sana",
-       pfpURL: "/images/sample-users/Minatozaki Sana",
-       email: "minatozaki_sana@dlsu.edu.ph",
-       description: "Lab Technician at a certain DLSU Lab. Lead vocalist of TWICE."
-   },
-   "park_jihyo@dlsu.edu.ph": {
-       name: "Park Ji-hyo",
-       pfpURL: "/images/sample-users/Park Ji-hyo",
-       email: "park_jihyo@dlsu.edu.ph",
-       description: "Student at DLSU University. Main vocalist and leader of TWICE."
-   }
+   // "im_nayeon@dlsu.edu.ph": {
+   //    name: "Im Na-yeon",
+   //    password: "1234",
+   //    pos: "student",
+   //    pfpURL: "/images/sample-users/Im Na-yeon",
+   //    email: "im_nayeon@dlsu.edu.ph",
+   //    description: "Student at DLSU University. Lead vocalist of TWICE."
+   // },
+   // "yoo_jeongyeon@dlsu.edu.ph": {
+   //    name: "Yoo Jeong-yeon",
+   //    password: "1234",
+   //    pos: "student",
+   //    pfpURL: "/images/sample-users/Yoo Jeong-yeon",
+   //    email: "yoo_jeongyeon@dlsu.edu.ph",
+   //    description: "Lab Technician at a certain DLSU Lab. Main vocalist of TWICE."
+   // },
+   // "hirai_momo@dlsu.edu.ph": {
+   //    name: "Hirai Momo",
+   //    password: "1234",
+   //    pos: "student",
+   //    pfpURL: "/images/sample-users/Hirai Momo",
+   //    email: "hirai_momo@dlsu.edu.ph",
+   //    description: "Lab Technician at a certain DLSU Lab. Main dancer of TWICE."
+   // },
+   // "minatozaki_sana@dlsu.edu.ph": {
+   //    name: "Minatozaki Sana",
+   //    password: "1234",
+   //    pos: "student",
+   //    pfpURL: "/images/sample-users/Minatozaki Sana",
+   //    email: "minatozaki_sana@dlsu.edu.ph",
+   //    description: "Lab Technician at a certain DLSU Lab. Lead vocalist of TWICE."
+   // },
+   // "park_jihyo@dlsu.edu.ph": {
+   //    name: "Park Ji-hyo",
+   //    password: "1234",
+   //    pos: "tech",
+   //    pfpURL: "/images/sample-users/Park Ji-hyo",
+   //    email: "park_jihyo@dlsu.edu.ph",
+   //    description: "Student at DLSU University. Main vocalist and leader of TWICE."
+   // }
 };
 
 server.listen(port);
